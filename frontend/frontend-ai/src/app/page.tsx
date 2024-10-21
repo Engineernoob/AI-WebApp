dcb23091949cecdbcfa1e0228169b4359e1f35dd
Taahirah Denmark <126827805+Engineernoob@users.noreply.github.com>
2024-10-21 13:51:15 -0500

    updating import error

--- frontend/frontend-ai/src/app/page.tsx
+++ frontend/frontend-ai/src/app/page.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 import Home from './pages/home';
 import Login from "./pages/login";
 import Chat from "./pages/aichat";
 import SignUp from "./pages/signup";
 import About from "./pages/about";
 
-
-
 function App() {
-  const router = useRouter();
+  const pathname = usePathname();
   const routes = [
     { path: '/', component: Home },
     { path: '/login', component: Login },
@@ -22,7 +20,7 @@ function App() {
     <div>
       {routes.map((route) => (
         <div key={route.path}>
-          {router.asPath === route.path && <route.component />}
+          {pathname === route.path && <route.component />}
         </div>
       ))}
     </div>
